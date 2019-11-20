4236e64d47d099b7a53ad8cec1cf3dbcf42d019c
markozg01 <marko.zg01@gmail.com>
2019-11-20 09:44:58 +0100

    - neki komit na feature grani

--- src/main/groovy/com/example/demo/DemoApplication.groovy
+++ src/main/groovy/com/example/demo/DemoApplication.groovy
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 class DemoApplication {
 
 	static void main(String[] args) {
+		//promenjeno na feature grani
 		SpringApplication.run(DemoApplication, args)
 	}
 
